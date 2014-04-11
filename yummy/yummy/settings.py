eb1d7d2733cf57fd85f8bb22e06f357b6725bdab
ghylxdw <ghylxdw@gmail.com>
2014-04-10 23:43:44 -0400

    add haystack and elasticsearch support

--- yummy/yummy/settings.py
+++ yummy/yummy/settings.py
@@ -38,7 +38,17 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'haystack',
 )
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
+    },
+} 
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
